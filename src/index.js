0a4d0457d5aa81241936adb548b11114ffeb890c
farhan0212 <farhan.ramadan0212@gmail.com>
2024-07-19 22:51:55 +0700

    setting prisma dan tambah endpoint /product

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.get("/api", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await prisma.product.findMany();
+  res.send(products);
+});
+
+app.listen(PORT, () => {
+  console.log("express API RUNNING in port : " + PORT);
+});
